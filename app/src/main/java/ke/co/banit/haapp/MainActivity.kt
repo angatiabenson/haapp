9e1bf3b3a888d590a560dc08474710ac5d537fda
Angatia Benson <bensonetia@gmail.com>
2024-06-03 05:46:48 -0500

    feat: added a top app bar for the app

--- app/src/main/java/ke/co/banit/haapp/MainActivity.kt
+++ app/src/main/java/ke/co/banit/haapp/MainActivity.kt
@@ -4,20 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -36,6 +47,9 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             HaappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        MyTopAppBar(navController)
+                    },
                     bottomBar = { BottomNavigationBar(navController) }
                 ) { innerPadding ->
                     Column(
@@ -49,6 +63,32 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MyTopAppBar(navController: NavHostController) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier.padding(start = 16.dp),
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Black
+            )
+        },
+        navigationIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.app_logo),
+                contentDescription = stringResource(id = R.string.app_logo_description),
+                modifier = Modifier
+                    .size(48.dp)
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+        )
+    )
 }
 
 @Composable
